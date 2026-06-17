740e630fa6e5223111a530f915f6f39e59e90da8
Somesh Pandey <spandey5368@gmail.com>
2026-06-17 17:10:30 +0530

    admin option

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // ✅ Must be '/' for Vercel
+  server: {
+    port: 5173,
+    host: true,
+  },
 })
